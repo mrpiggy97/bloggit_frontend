4f0bce5818e2bf9a51458a1ced3a66bf397f2ba6
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-03 15:27:29 -0400

    created basic app layouot

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="header">
+      <main-header></main-header>
     </div>
-    <router-view/>
+
+    <div id="posts">
+    </div>
+
+    <div id="footer">
+
+    </div>
   </div>
 </template>
 
+<script>
+import MainHeader from '@/components/MainHeader'
+import getPosts from '@/services/getPosts'
+export default {
+
+  name: 'App',
+
+  components:{
+    MainHeader,
+  },
+
+  methods:{
+    async retrievePosts(){
+      try{
+        let response = await getPosts()
+        console.log(response.data)
+      }
+
+      catch(error){
+        console.log(error)
+      }
+    }
+  },
+
+  created(){
+    this.retrievePosts()
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import './App.css';
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="main-header">
+      <span class="shit">icon here</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import './css/MainHeader.css';
+</style>
