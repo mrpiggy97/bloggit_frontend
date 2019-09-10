f875b85df34630733ea5b9aecb29959884ff8949
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-09 23:30:36 -0400

    created new view and added it to the router

---
+++ src/views/Authentication.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="authentication">
+      <login-form v-if="login"></login-form>
+      <register-form v-else-if="register"></register-form>
+  </div>
+</template>
+
+<script>
+import LoginForm from "@/components/authentication/LoginForm"
+import RegisterForm from "@/components/authentication/RegisterForm"
+export default {
+    name: 'Authentication',
+
+    components:{
+        LoginForm,
+        RegisterForm
+    },
+
+    data(){
+        return{
+            //action should only be login or register
+            register: this.$route.params.action == 'register',
+            login: this.$route.params.action == 'login'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
