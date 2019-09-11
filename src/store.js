e4e8b34e0870e5725997ebc362d0bdfd566f6b3f
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-11 00:42:26 -0400

    fixed bug for token

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    authenticated: false,
+    authenticated: window.localStorage.getItem('token'),
   },
   mutations: {
     setAuthenticated(state, payload){
