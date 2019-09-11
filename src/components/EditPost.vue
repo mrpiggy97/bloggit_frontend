8e96655b56ebf65fe6c493c28d421f172194c2b1
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-11 00:23:10 -0400

    created components for post one for reading and the other for editing

---
+++ src/components/EditPost.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="edit-post">
+        <div class="post-header">
+            <span class="post-owner">{{owner}}</span>
+            <span class="post-date">{{date_posted}}</span>
+        </div>
+        <form class="post-form-edit">
+            <textarea class="post-text" v-model="text"></textarea>
+            <button type="submit">edit</button>            
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EditPost',
+    
+    props:{
+        info: {type: Object, required: true}
+    },
+
+    data(){
+        return{
+            owner: this.props.info.owner,
+            date_posted: this.props.info.date,
+            text: this.props.info.text,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PostInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="post-info">
+      <div class="post-header">
+          <span class="username">{{username}}</span>
+          <span class="date-posted">{{date_posted}}</span>
+      </div>
+
+      <div class="post-text">
+          <span class="text">{{text}}</span>
+      </div>
+
+      <div class="post-communities">
+          <span v-for="com in communities" :key="com.slug">{{com.slug}}</span>
+      </div>
+
+      <div class="post-social">
+          <span class="likes">{{likes}}</span>
+          <span class="reports">{{reports}}</span>
+          <span :class="['like', liked ? 'active' : 'inactive']">like</span>
+          <span :class="['report', reported ? 'active' : 'inactive']">report</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PostInfo',
+
+    props:{
+        info: {type: Object, required: true}
+    },
+
+    data(){
+        return{
+            owner: this.props.info.owner,
+            date_posted: this.props.info.date,
+            title: this.props.info.title,
+            text: this.props.info.text,
+            communities: this.props.info.communities_list,
+            likes: this.props.info.likes,
+            reports: this.props.info.reports,
+            liked: this.props.info.liked,
+            reported: this.props.info.reported,
+            uuid: this.props.info.uuid
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
