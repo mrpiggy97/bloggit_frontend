e84d4ee7cd0bac6252751f005766a24b646cc5cf
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-11 23:30:45 -0400

    created service for post creation

---
+++ src/services/makePost.js
@@ -0,0 +1,15 @@
+import authenticateTokenInstance from '@/services/axiosInstances'
+
+const makePost = (data) => {
+    return authenticateTokenInstance({
+        method: 'POST',
+        url: '/posts/make-post/',
+        headers:{
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        data: JSON.stringify(data)
+    })
+}
+
+export default makePost
