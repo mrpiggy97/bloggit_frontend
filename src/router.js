53f4114299eff70dd8be88cf188967c55de9e563
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-13 17:48:21 -0400

    added new path

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Authentication from './views/Authentication.vue'
+import CreatePost from './views/CreatePost.vue'
 
 Vue.use(Router)
 
@@ -26,6 +27,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/create-post',
+      name: 'CreatePost',
+      component: CreatePost
     }
   ]
 })
