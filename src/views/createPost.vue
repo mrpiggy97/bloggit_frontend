bf1f842cd66e88a3d5c0cf6b04fa09b0ab68058d
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-13 17:31:20 -0400

    created view for post creation

---
+++ src/views/createPost.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="create-post">
+        <form class="post-form" @submit="createPost">
+            <label for="title">title</label>
+            <input type="text" id="title" maxlength="200" v-model="title"/>
+            <label for="text">text</label>
+            <textarea  id="text" cols="30" rows="10" maxlength="700" v-model="text"></textarea>
+            <label for="communities">communities separated by comas (,)</label>
+            <input type="text" id="communities" maxlength="100" v-model="communities"/>
+            <button type="submit">post</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import makePost from '@/services/makePost'
+
+export default {
+    name: 'CreatePost',
+
+    data(){
+        return{
+            title: '',
+            text: '',
+            communities: ''
+        }
+    },
+
+    computed:{
+        
+        postData(){
+            return{
+                title: this.title,
+                text: this.text,
+                add_communities: this.communities.split(",")
+            }
+        }
+    },
+
+    methods:{
+        async create(e){
+            e.preventDefault()
+            try{
+                let response = await makePost(this.postData)
+                console.log(response)
+                this.title = ''
+                this.text = ''
+                this.communities = ''
+            }
+
+            catch(error){
+                console.log(error)
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
