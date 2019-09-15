9248d9def77d15e288afddb90e0a794e39c70a3b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-15 17:38:28 -0400

    changed token name

--- src/store.js
+++ src/store.js
@@ -5,15 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    authenticated: window.localStorage.getItem('token'),
+    authenticated: window.localStorage.getItem('bloggit_token'),
   },
   mutations: {
     setAuthenticated(state, payload, token){
       if(payload === false){
-        window.localStorage.removeItem('token')
+        window.localStorage.removeItem('bloggit_token')
       }
       else if(payload === true){
-        window.localStorage.setItem('token', token)
+        window.localStorage.setItem('bloggit_token', token)
       }
       state.authenticated = payload
     }
