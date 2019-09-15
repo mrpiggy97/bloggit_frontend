ac5ac8431945afbb90d619fe6011d6d85b260812
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-09-15 16:33:57 -0400

    added first layout

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="main-header">
       <span class="shit">icon here</span>
+      <span @click="redirectLogin">login</span>
+      <span @click="redirectRegister">register</span>
+      <span @click="redirectCreatePost">create post</span>
+      <span v-if="authenticated" @click="makeLogout">logout</span>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState, mapMutations } from 'vuex'
 
+import { logout } from '@/services/authentication'
+
+export default {
+  name: 'MainHeader',
+
+  computed:{
+    ...mapState(['authenticated'])
+  },
+
+  methods:{
+    ...mapMutations(['setAuthenticated']),
+    redirectCreatePost(){
+      this.$router.history.push({name: 'CreatePost'})
+    },
+    redirectLogin(){
+      this.$router.history.push({path: '/auth/login'})
+    },
+    redirectRegister(){
+      this.$router.history.push({path: '/auth/register'})
+    },
+
+    async makeLogout(){
+      try{
+        let response = await logout()
+        console.log(response)
+        this.setAuthenticated(false, null) //new authenticated state and token
+      }
+      catch(error){
+        console.log(error.request.status)
+      }
+    }
+  }
 }
 </script>
 
