cd651a14487d6df16c48bc500a3d9e0e1ae05cb0
mrpiggy97 <fabyjesusrivas10@gmail.com>
2019-10-01 16:59:45 -0400

    changed authenticated to be a boolean value and now the app sets a username given from backend in localStorage

--- src/store.js
+++ src/store.js
@@ -1,24 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { login, logout } from '@/services/authentication'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    authenticated: window.localStorage.getItem('bloggit_token'),
+    authenticated: window.localStorage.getItem('bloggit_token') ? true : false,
   },
   mutations: {
     setAuthenticated(state, payload){
       if(payload.newState === false){
         window.localStorage.removeItem('bloggit_token')
+        window.localStorage.removeItem('bloggit_username')
       }
       else if(payload.newState === true){
         window.localStorage.setItem('bloggit_token', payload.token)
+        window.localStorage.setItem('bloggit_username', payload.username)
       }
       state.authenticated = payload.newState
     }
   },
   actions: {
-
   }
 })
