717b5f66ccfb638b8cacaec4398361dc16123ae7
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-03 02:08:34 -0500

    added argument page with default as 1 to function and applied that to url

--- src/services/PostServices/getPosts.js
+++ src/services/PostServices/getPosts.js
@@ -1,18 +1,18 @@
 import { authenticateTokenInstance, withoutTokenInstance } from '../axiosInstances'
 
-const getPosts = () => {
+const getPosts = (page = 1) => {
     if(window.localStorage.getItem('bloggit_token')){
         console.log("getPosts service console log")
         console.log(window.localStorage.getItem('bloggit_token'))
         return authenticateTokenInstance({
             method: 'GET',
-            url: '/posts/',
+            url: `/posts/?page=${page}/`,
         })        
     }
     else{
         return withoutTokenInstance({
             method: 'GET',
-            url: '/posts/'
+            url: `/posts/?page=${page}/`
         })
     }
 }
