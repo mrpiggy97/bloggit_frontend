f735dda0bd139a0ea8245109358c6ecd75c5cb0d
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-06 22:34:17 -0500

    changed css and some items in MainHeader component

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,8 +1,18 @@
 <template>
-  <div id="main-header-component">
-    <span>home</span>
-    <span>user</span>
-    <span>login</span>
+  <div id="main-header">
+    <div id="basic-menu">
+      <span @click="redirectHome">home</span>
+      <input type="text" name="search" id="search" class="search" maxlength="100"/>
+      <span>communities</span>
+      <span>github</span>
+    </div>
+
+    <div id="user">
+      <span v-if="authenticated">user</span>
+      <span v-if="authenticated">logout</span>
+      <span v-if="!authenticated" @click="redirectLogin">login</span>
+      <span v-if="!authenticated" @click="redirectRegister">register</span>
+    </div>
   </div>
 </template>
 
@@ -19,16 +29,37 @@ export default {
   },
 
   methods:{
-    ...mapMutations(['logUserOut']),
+    ...mapMutations(['removeUserCredentials']),
+
     redirectCreatePost(){
       this.$router.history.push({name: 'CreatePost'})
     },
+
+    redirectHome(){
+      this.$router.history.push({name: 'home'})
+    },
+
     redirectLogin(){
-      this.$router.history.push({path: '/auth/login'})
+      this.$router.history.push({name: 'authentication', params: {action: 'login'}})
     },
+
     redirectRegister(){
-      this.$router.history.push({path: '/auth/register'})
+      this.$router.history.push({name: 'authentication', params: {action: 'register'}})
     },
+
+    async makeLogout(){
+      if(this.authenticated){
+        try{
+          await logout()
+          this.removeUserCredentials()
+        }
+        catch(error){
+          console.log("error at MainHeader component at makeLogout method")
+          console.log("beggining at line 34");
+          console.log(error.request.status);
+        }
+      }
+    }
   }
 }
 </script>
