1e139b595c48d2c0649e940f11ff541e11b17fa8
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-15 13:35:13 -0500

    deleted setAuthenticated mutation, added username, profilePic and userCommunities to state, and applied does new state additions to mutations

--- src/store.js
+++ src/store.js
@@ -6,35 +6,31 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     authenticated: window.localStorage.getItem('bloggit_token') ? true : false,
+    username: window.localStorage.getItem('bloggit_username'),
+    profilePic: window.localStorage.getItem('bloggit_profile_pic'),
+    userCommunities: window.localStorage.getItem('bloggit_user_communities')
   },
   mutations: {
-    setAuthenticated(state, payload){
-      if(payload.newState === false){
-        window.localStorage.removeItem('bloggit_token')
-        window.localStorage.removeItem('bloggit_username')
-        window.localStorage.removeItem('bloggit_profile_pic')
-      }
-      else if(payload.newState === true){
-        window.localStorage.setItem('bloggit_token', payload.token)
-        window.localStorage.setItem('bloggit_username', payload.username)
-        window.localStorage.removeItem('bloggit_profile_pic', payload.profile_pic)
-
-      }
-      state.authenticated = payload.newState
-    },
-
     removeUserCredentials(state){
       window.localStorage.removeItem('bloggit_token')
       window.localStorage.removeItem('bloggit_username')
       window.localStorage.removeItem('bloggit_profile_pic')
+      window.localStorage.removeItem('bloggit_user_communities')
       state.authenticated = false
+      state.username = null
+      state.profilePic = null
+      state.userCommunities = null
     },
 
     setUserCredentials(state, payload){
       window.localStorage.setItem('bloggit_token', payload.token)
       window.localStorage.setItem('bloggit_username', payload.username)
       window.localStorage.setItem('bloggit_profile_pic', payload.profile_pic)
+      window.localStorage.setItem('bloggit_user_communities', payload.communities)
       state.authenticated = true
+      state.username = payload.username
+      state.profilePic = payload.profile_pic
+      state.userCommunities = payload.communities
     }
   },
   actions: {
