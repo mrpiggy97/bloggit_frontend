06b38b7daa916fb38215d3da49f5ad5d433de081
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-22 12:41:34 -0500

    added nextPage and previousPage to data, changed name of function that retrieves posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="home-view">
+    <span v-if="previousPage" @click="getPagePosts(previousPage)">previous</span>
+    <span v-if="nextPage" @click="getPagePosts(nextPage)">next</span>
     <post-info v-for="post in posts" :key="post.uuid" :info="post"
-    :previewMode="true"
-    />
+    :previewMode="true"/>
+    <span v-if="previousPage" @click="getPagePosts(previousPage)">previous</span>
+    <span v-if="nextPage" @click="getPagePosts(nextPage)">next</span>
   </div>
 </template>
 
@@ -21,56 +24,26 @@ export default {
 
   data(){
     return{
-      posts: [
-        {
-          date: '12 may 2019',
-          title: 'this is the title i hope this works goddamit shit men goddamit'+
-          'work worksdald sa;d ;slkdsa s;lkdal;sdk laskdla;sdk laskdl laskdldk '+
-          'lad;ad;as adm;alsd ;laskd;la sd ;laskd  ;asldk ;lask d;asl d;laskd;la',
-          text: 'this is the the post i hope it work',
-          communities: ['this is the community let see how long can it be,'],
-          likes: 1,
-          reports: 0,
-          liked: null,
-          reported: null,
-          uuid: '1412sdfsf1223vefdgbdfb3',
-          owner: {
-            username: 'thisustheusername',
-            profile_pic: null
-          },
-        },
-        {
-        date: '12 may 2019',
-        title: 'this is the title i hope this works goddamit shit men goddamit'+
-        'work worksdald sa;d ;slkdsa s;lkdal;sdk laskdla;sdk laskdl laskdldk '+
-        'lad;ad;as adm;alsd ;laskd;la sd ;laskd  ;asldk ;lask d;asl d;laskd;la',
-        text: 'this is the the post i hope it work',
-        communities: ['this is the community let see how long can it be before'],
-        likes: 1,
-        reports: 0,
-        liked: null,
-        reported: null,
-        uuid: '1412sdfsf1223vsssefdgbdfb3',
-        owner: { username: 'thisustheusername', profile_pic: null}
-        }
-      ],
+      posts: [],
+      nextPage: null,
+      previousPage: null
     }
   },
 
   computed:{
-    ...mapState(['authenticated'])
+    ...mapState(['authenticated']),
   },
 
   methods:{
     ...mapMutations(['removeUserCredentials']),
 
-    async presentApp(){
+    async getPagePosts(page){
       
       try{
-        let response = await getPosts()
-        this.posts = response.data.results     
-        console.log(response.data.authenticated)
-        console.log(this.authenticated)
+        let response = await getPosts(page)
+        this.posts = response.data.results
+        this.nextPage = response.next_page
+        this.previousPage = response.previous_page
 
         if(response.data.authenticated !== this.authenticated){
           this.removeUserCredentials()
@@ -78,8 +51,7 @@ export default {
       }
 
       catch(error){
-        console.log("error at Home view in presentApp method beggining at line")
-        console.log("32")
+        console.log("error at Home view in presentApp method")
         console.log(error)
       }
       finally{
@@ -87,6 +59,10 @@ export default {
       }
     },
   },
+
+  created(){
+    this.getPagePosts(1)//1 for inital posts
+  }
 }
 </script>
 
