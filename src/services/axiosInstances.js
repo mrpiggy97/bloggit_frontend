9d9cd39e5b9e964ab30a2a431e3e2a1cc2f8d7b2
mrpiggy97 <fabyjesusrivas10@gmail.com>
2020-01-26 17:28:31 -0500

    added constant for api url

--- src/services/axiosInstances.js
+++ src/services/axiosInstances.js
@@ -1,17 +1,19 @@
 import axios from 'axios'
 
+const BASEAPIURL = process.env.VUE_APP_BLOGGIT_API_DEV_URL
+
 //basically only endpoints that will not require or expect a token
 //to authenticate request will use this function
 
 export const withoutTokenInstance = axios.create({
-    baseURL: process.env.VUE_APP_BLOGGIT_API_DEV_URL,
+    baseURL: BASEAPIURL,
     timeout: 5000
 })
 
 //all other endpoints that can and will check if request is authenticated should
 //use this axios instance
 export const authenticateTokenInstance = axios.create({
-    baseURL: process.env.VUE_APP_BLOGGIT_API_DEV_URL,
+    baseURL: BASEAPIURL,
     headers:{
         'Authorization': `JWT ${window.localStorage.getItem('bloggit_token')}`
     },
@@ -19,7 +21,7 @@ export const authenticateTokenInstance = axios.create({
 })
 
 export const CreateModifyInstance = axios.create({
-    baseURL: process.env.VUE_APP_BLOGGIT_API_DEV_URL,
+    baseURL: BASEAPIURL,
     headers:{
         'Content-type': 'application/json',
         'accept': 'application/json',
